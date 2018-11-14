7b287f3cbaea462e4fe51998ed481cf3396ff757
Calvin Nesbitt <c.f.nesbitt@student.reading.ac.uk>
2018-11-14 20:25:47 +0000

    Order of convergence tested as working. All scripts now finished. Need to move to single script and clean. Particular consideration for making plots look nice.

--- orderOfConvergence.py
+++ orderOfConvergence.py
@@ -10,58 +10,57 @@ from diagnostics import *
 # Parameters
 xmin = 0
 xmax = 1
-nx_values = np.arange(2, 100, 1)
-dx_values = (xmax - xmin)/nx_values # Calculate spatial step
 c = 0.5
 
-# Error calculation for FTBS scheme
-error_Matrix = np.zeros((len(dx_values), 4)) # Matrix to store errors
+# Values to test over
+nt_values = np.arange(20, 80, 4) # Array to story different number of time steps
+nx_values = np.arange(10, 40, 2) # Adjust nx to fix courant number
+dx_values = (xmax - xmin)/nx_values # Calculate spatial step
+
+# Error calcualtion for each scheme
+
+error_Matrix = np.zeros(len(dx_values)) # Matrix to store errors
+
+# FTBS scheme
 for i,dx in enumerate(dx_values):
     x = np.arange(xmin, xmax, dx)
     phiOld = cosBell(x, 0, 0.75) # Initial condition for given discretisation
-    nt = int(nx_values[i] / c ) # nt must be changed that we run the scheme for same amount of time t
+    nt = nt_values[i] # nt must be changed that we run the scheme for same amount of time t
     phiFTBS = FTBS(phiOld.copy(), c, nt) # Run the scheme for each (c, nt) pair
-    error_Matrix[i, 0] = dx_values[i] # Store dx value
-    phiAnalytic = cosBell((x - c*nt*dx_values[i])%(xmax - xmin), 0, 0.75)
-    error_Matrix[i, 1] = l2ErrorNorm(phiFTBS, phiAnalytic) # Store L2 errors
-np.savetxt('results/convergence_analysis/FTBS_Errors.csv',error_Matrix, delimiter=',', newline='\n', header='Results of running FTBS scheme for different spatial discretisation')
-plt.plot(dx_values, error_Matrix[:, 1],'--g^', label = 'FTBS')
+    phiAnalytic = cosBell((x - c*nt*dx)%(xmax - xmin), 0, 0.75)
+    error_Matrix[i] = l2ErrorNorm(phiFTBS, phiAnalytic) # Store L2 errors
+plt.plot(dx_values, error_Matrix,'--g^', label = 'FTBS')
 
-# Error calculation for FTCS scheme
+# BTCS scheme
 for i,dx in enumerate(dx_values):
     x = np.arange(xmin, xmax, dx)
     phiOld = cosBell(x, 0, 0.75) # Initial condition for given discretisation
-    nt = int(nx_values[i] / c ) # nt must be changed that we run the scheme for same amount of time t
-    phiFTCS = FTCS(phiOld.copy(), c, nt) # Run the scheme for each (c, nt) pair
-    error_Matrix[i, 0] = dx_values[i] # Store dx value
-    phiAnalytic = cosBell((x - c*nt*dx_values[i])%(xmax - xmin), 0, 0.75)
-    error_Matrix[i, 1] = l2ErrorNorm(phiFTCS, phiAnalytic) # Store L2 errors
-np.savetxt('results/convergence_analysis/FTCS_Errors.csv',error_Matrix, delimiter=',', newline='\n', header='Results of running FTBS scheme for different spatial discretisation')
-plt.plot(dx_values, error_Matrix[:, 1],'--r^', label = 'FTCS')
-
-# Error calculation for BTCS scheme
-for i,dx in enumerate(dx_values):
-    x = np.arange(xmin, xmax, dx)
-    phiOld = cosBell(x, 0, 0.75) # Initial condition for given discretisation
-    nt = int(nx_values[i] / c ) # nt must be changed that we run the scheme for same amount of time t
+    nt = nt_values[i] # nt must be changed that we run the scheme for same amount of time t
     phiBTCS = BTCS(phiOld.copy(), c, nt) # Run the scheme for each (c, nt) pair
-    error_Matrix[i, 0] = dx_values[i] # Store dx value
-    phiAnalytic = cosBell((x - c*nt*dx_values[i])%(xmax - xmin), 0, 0.75)
-    error_Matrix[i, 1] = l2ErrorNorm(phiBTCS, phiAnalytic) # Store L2 errors
-np.savetxt('results/convergence_analysis/BTCS_Errors.csv',error_Matrix, delimiter=',', newline='\n', header='Results of running FTBS scheme for different spatial discretisation')
-plt.plot(dx_values, error_Matrix[:, 1],'--c^', label = 'BTCS')
+    phiAnalytic = cosBell((x - c*nt*dx)%(xmax - xmin), 0, 0.75)
+    error_Matrix[i] = l2ErrorNorm(phiBTCS, phiAnalytic) # Store L2 errors
+plt.plot(dx_values, error_Matrix,'--r^', label = 'BTCS')
 
-# Error calculation for CTCS scheme
+# CTCS scheme
 for i,dx in enumerate(dx_values):
     x = np.arange(xmin, xmax, dx)
     phiOld = cosBell(x, 0, 0.75) # Initial condition for given discretisation
-    nt = int(nx_values[i] / c ) # nt must be changed that we run the scheme for same amount of time t
+    nt = nt_values[i] # nt must be changed that we run the scheme for same amount of time t
     phiCTCS = CTCS(phiOld.copy(), c, nt) # Run the scheme for each (c, nt) pair
-    error_Matrix[i, 0] = dx_values[i] # Store dx value
-    phiAnalytic = cosBell((x - c*nt*dx_values[i])%(xmax - xmin), 0, 0.75)
-    error_Matrix[i, 1] = l2ErrorNorm(phiCTCS, phiAnalytic) # Store L2 errors
-np.savetxt('results/convergence_analysis/CTCS_Errors.csv',error_Matrix, delimiter=',', newline='\n', header='Results of running FTBS scheme for different spatial discretisation')
-plt.plot(dx_values, error_Matrix[:, 1],'--m^', label = 'CTCS')
+    phiAnalytic = cosBell((x - c*nt*dx)%(xmax - xmin), 0, 0.75)
+    error_Matrix[i] = l2ErrorNorm(phiCTCS, phiAnalytic) # Store L2 errors
+plt.loglog(dx_values, error_Matrix,'--m^', label = 'CTCS')
+
+# SL scheme
+for i,dx in enumerate(dx_values):
+    if nt < 60: # Stopping scheme running for too many timesteps
+        x = np.arange(xmin, xmax, dx)
+        phiOld = cosBell(x, 0, 0.75) # Initial condition for given discretisation
+        nt = nt_values[i] # nt must be changed that we run the scheme for same amount of time t
+        phi_sem_lag = sem_lag(phiOld.copy(), c, nt) # Run the scheme for each (c, nt) pair
+        phiAnalytic = cosBell((x - c*nt*dx)%(xmax - xmin), 0, 0.75)
+        error_Matrix[i] = l2ErrorNorm(phi_sem_lag, phiAnalytic) # Store L2 errors
+plt.loglog(dx_values, error_Matrix,'--k^', label = 'SL')
 
 #Plot Details
 plt.plot(dx_values, dx_values, label = 'x')
